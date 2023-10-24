945236da627023cc1e4c2e471a71feeb1a7ac91a
Xiaohua Peng <609370075@qq.com>
2023-10-24 19:34:16 +0800

    feat: 优化接口请求代码

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -9,20 +9,10 @@ import openai
 import requests
 import json
 
+# 设置OpenAI API端点
 OPENAI_API_URL = "https://api.openai.com/v1/engines/davinci/completions"
+# 设置OpenAI API密钥
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
-
-def get_openai_suggestions(prompt):
-    headers = {
-        "Authorization": f"Bearer {OPENAI_API_KEY}",
-        "Content-Type": "application/json"
-    }
-    data = {
-        "prompt": prompt,
-        "max_tokens": 150
-    }
-    response = requests.post(OPENAI_API_URL, headers=headers, json=data)
-    return response.json().get('choices')[0].get('text').strip()
 
 
 @app.route('/')
@@ -86,43 +76,10 @@ def get_count():
 
 @app.route('/api/evaluate_resume', methods=['POST'])
 def evaluate_resume():
-
-    # 打印原始请求数据和解析后的表单数据
-    print(request.data)  # 打印原始请求数据
-    print(request.form)  # 打印解析后的表单数据
-
-    openai.api_key = os.environ.get('OPENAI_API_KEY')
-
-    # resume_text = request.form.get('resume_text')
-    # if not resume_text:
-    #     return jsonify({"error": "Resume text is required"}), 400
-
-    # prompt = f"请评估以下简历内容: {resume_text}"
-
-    # 准备请求的数据
-    request_data = {
-        "engine": "davinci",  # 使用gpt-3.5-turbo引擎
-        "prompt": "Translate the following English text to French: 'Hello, how are you?'",  # 这里是您的输入文本
-        "max_tokens": 500  # 生成的文本的最大长度
-    }
-
-    # 将请求数据转换为JSON格式
-    json_data = json.dumps(request_data)
-
-    # 设置请求的content-type为application/json
-    headers = {"Content-Type": "application/json"}
-
-    # 使用OpenAI的Completion API生成文本
-    # response = openai.Completion.create(data=json_data, headers=headers)
-
     q = "用python实现：提示手动输入3个不同的3位数区间，输入结束后计算这3个区间的交集，并输出结果区间"
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-                {"role": "system", "content": "一个有10年Python开发经验的资深算法工程师"},
-                {"role": "user", "content": q}
-            ]
+    response = openai.Completion.create(
+        engine="gpt-3.5-turbo",  # 使用Davinci引擎，您可以根据需要选择其他引擎
+        prompt=q
     )
 
-
-    return response.choices[0].text.strip()  # 返回生成的文本
+    return response.choices[0].text.strip()
